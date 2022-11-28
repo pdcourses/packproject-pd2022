aac42cc2511118fc69bd1350eaef1f6044f2c672
pdcourses <freshitpt@gmail.com>
2022-11-28 20:43:59 +0200

    all

---
+++ server/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+// const router = require('./router.js');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+//app.use('/api', router);
+
+module.exports = app;
---
+++ server/server.js
@@ -0,0 +1,6 @@
+const http = require('http');
+const app = require('./app');
+const server = http.createServer(app);
+const PORT = process.env.PORT || 5000;
+const HOST = process.env.HOST || '127.0.0.1';
+server.listen(PORT, HOST, () => console.log('Server is listenong...'));
